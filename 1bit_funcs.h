318d4c9e97505fc40916228cabe3ef6ab7674bde
KaspianTC <kaspian.thoft.christensen@mail.utoronto.ca>
2026-03-01 01:18:19 -0500

    Cleaned up some unnecessary comments

--- 1bit_funcs.h
+++ 1bit_funcs.h
@@ -16,14 +16,6 @@ typedef struct {
 #endif
 
 void freeOneImage(OneImage *om);
-/* void imgThreshholdMapDither(Image * img,int * threshhold_map,int x_size,int y_size,int threshhold_range);
-void imgBayerZero(Image * img);
-void imgBayerOne(Image * img);
-void imgBayerTwo(Image * img);
-void imgErrorDither(Image * img, int factor,float *errorKernel,int *locationPositions,int kernelSize);
-void ditherFloydSteinberg(Image * img, int factor);
-void ditherAtkinson(Image * img, int factor);
-void ditherJarvisJudiceNinke(Image * img, int factor); */
 Image * imgGrayscale(Image * img);
 OneImage* convertImgToOne(Image *img);
 void averageColourImage(Image* img);
